bd86bfceff15bdb346b2dcb74b81372d5f2919b5
Michael Nadeau <waaverecords@gmail.com>
2023-12-29 08:49:26 -0500

    List and play tracks in search results.

--- BuildAndInstallPlugin.bat
+++ BuildAndInstallPlugin.bat
@@ -1,5 +1,5 @@
 
 dotnet build -c Release
 taskkill /F /IM PowerToys.exe
-robocopy /e /w:5 "C:\Users\m_sno\PowerToys-Run-Spotify\bin\Release\net7.0-windows" "C:\Program Files\PowerToys\RunPlugins\PowerToys-Run-Spotify"
-start "" "C:\Program Files\PowerToys\PowerToys.exe"
+robocopy /e /w:5 "C:\Users\m_sno\PowerToys-Run-Spotify\bin\Release\net8.0-windows" "C:\Users\m_sno\PowerToys\x64\Release\RunPlugins\PowerToys-Run-Spotify"
+start "" "C:\Users\m_sno\PowerToys\x64\Release\PowerToys.exe"
--- Main.cs
+++ Main.cs
@@ -1,9 +1,9 @@
-﻿using System.Windows.Input;
-using Wox.Plugin;
+﻿using Wox.Plugin;
 using System.Windows.Controls;
 using Microsoft.PowerToys.Settings.UI.Library;
 using System.Windows.Media.Imaging;
 using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -61,20 +61,30 @@ public class Main : IPlugin, IContextMenu, ISettingProvider
 
     public List<Result> Query(Query query)
     {
+        if (string.IsNullOrEmpty(ClientId))
+            return new List<Result>() {new Result
+            {
+                Title = "Spotify - Missing client ID",
+                SubTitle = "Set your client ID in the plugin's settings",
+                Action = context => true
+            }};
+
+        if (!File.Exists(_credentialsPath))
+            return new List<Result>() {new Result
+                {
+                    Title = "Spotify - Login",
+                    SubTitle = "Login to authorize the use of the Spotify API",
+                    Action = context =>
+                    {
+                        LoginToSpotify(ClientId);
+                        return true;
+                    }
+                }};
+
         var results = new List<Result>();
-
-        results.Add(new Result
-        {
-            Title = "Client Id",
-            SubTitle = "test",
-            Icon = () => new BitmapImage(new Uri("https://i.scdn.co/image/ab67616d0000485163ff604088796d31f7ebef33"))
-        });
-
-        // if (_spotifyClient  == null && string.IsNullOrEmpty(ClientId))
-        //     return results;
         
-        // if (_spotifyClient == null)
-        //     _spotifyClient = GetSpotifyClient(ClientId).GetAwaiter().GetResult();
+        if (_spotifyClient == null)
+            _spotifyClient = GetSpotifyClient(ClientId).GetAwaiter().GetResult();
 
         // var playback = _spotifyClient.Player.GetCurrentPlayback().GetAwaiter().GetResult();
         // if (playback?.Item is FullTrack track)
@@ -85,6 +95,24 @@ public class Main : IPlugin, IContextMenu, ISettingProvider
         //         Icon = () => new BitmapImage(new Uri(track.Album.Images.OrderBy(x => x.Width * x.Height).First().Url))
         //     });
 
+        var searchRequest = new SearchRequest(SearchRequest.Types.All, query.Search);
+        var searchResponse = _spotifyClient.Search.Item(searchRequest).GetAwaiter().GetResult();
+
+        if (searchResponse.Tracks.Items != null)
+            results.AddRange(searchResponse.Tracks.Items.Select(track => new Result
+            {
+                Title = track.Name,
+                SubTitle = string.Join(", ", track.Artists.Select(x => x.Name)),
+                Icon = () => new BitmapImage(new Uri(track.Album.Images.OrderBy(x => x.Width * x.Height).First().Url)),
+                Action = context =>
+                {
+                    _spotifyClient.Player.ResumePlayback(new PlayerResumePlaybackRequest{
+                        Uris = new List<string> { track.Uri }
+                    });
+                    return true;
+                }
+            }));
+
         return results;
     }
 
@@ -92,84 +120,83 @@ public class Main : IPlugin, IContextMenu, ISettingProvider
     {
         return new List<ContextMenuResult>
         {
-            new ContextMenuResult
-            {
-                Title = "Play",
-                Glyph = "\xE768",
-                FontFamily = "Segoe MDL2 Assets",
-                AcceleratorKey = Key.P,
-                AcceleratorModifiers = ModifierKeys.Control,
-                Action = e => true,
-            },
-            new ContextMenuResult
-            {
-                Title = "Pause",
-                Glyph = "\xE769",
-                FontFamily = "Segoe MDL2 Assets",
-                AcceleratorKey = Key.C,
-                AcceleratorModifiers = ModifierKeys.Control,
-                Action = e => true,
-            }
+            // new ContextMenuResult
+            // {
+            //     Title = "Play",
+            //     Glyph = "\xE768",
+            //     FontFamily = "Segoe MDL2 Assets",
+            //     AcceleratorKey = Key.P,
+            //     AcceleratorModifiers = ModifierKeys.Control,
+            //     Action = e => true,
+            // },
+            // new ContextMenuResult
+            // {
+            //     Title = "Pause",
+            //     Glyph = "\xE769",
+            //     FontFamily = "Segoe MDL2 Assets",
+            //     AcceleratorKey = Key.C,
+            //     AcceleratorModifiers = ModifierKeys.Control,
+            //     Action = e => true,
+            // }
         };
     }
 
-    // private async Task LoginToSpotify(string clientId)
-    // {
-        // var (verifier, challenge) = PKCEUtil.GenerateCodes();
+    private async Task LoginToSpotify(string clientId)
+    {
+        var (verifier, challenge) = PKCEUtil.GenerateCodes();
 
-        // var tcs = new TaskCompletionSource();
+        var tcs = new TaskCompletionSource();
 
-        // var callbackUri = new Uri("http://localhost:5543/callback");
-        // var authServer = new EmbedIOAuthServer(callbackUri, 5543);
+        var callbackUri = new Uri("http://localhost:5543/callback");
+        var authServer = new EmbedIOAuthServer(callbackUri, 5543);
 
-        // authServer.AuthorizationCodeReceived += async (sender, response) =>
-        // {
-        //     await authServer.Stop();
-        //     // var a = new PKCETokenRequest(clientId, response.Code, authServer.BaseUri, verifier);
-        //     // var o = new OAuthClient();
-        //     //var tokenResponse = await o.RequestToken(a);// this line is causing the error
-        //     // await File.WriteAllTextAsync(_credentialsPath, JsonConvert.SerializeObject(tokenResponse));
-        //     // tcs.SetResult();
-        // };
+        authServer.AuthorizationCodeReceived += async (sender, response) =>
+        {
+            await authServer.Stop();
+            var tokenRequest = new PKCETokenRequest(clientId, response.Code, authServer.BaseUri, verifier);
+            var client = new OAuthClient();
+            var tokenResponse = await client.RequestToken(tokenRequest);
+            await File.WriteAllTextAsync(_credentialsPath, JsonConvert.SerializeObject(tokenResponse));
+            tcs.SetResult();
+        };
 
-        // await authServer.Start();
+        await authServer.Start();
 
-        // // var request = new LoginRequest(authServer.BaseUri, clientId, LoginRequest.ResponseType.Code)
-        // // {
-        // //     CodeChallenge = challenge,
-        // //     CodeChallengeMethod = "S256",
-        // //     Scope = new List<string>
-        // //     {
-        // //         Scopes.UserReadPlaybackState,
-        // //         Scopes.UserModifyPlaybackState
-        // //     }
-        // // };
+        var loginRequest = new LoginRequest(authServer.BaseUri, clientId, LoginRequest.ResponseType.Code)
+        {
+            CodeChallenge = challenge,
+            CodeChallengeMethod = "S256",
+            Scope = new List<string>
+            {
+                Scopes.UserReadPlaybackState,
+                Scopes.UserModifyPlaybackState
+            }
+        };
 
-        // // var uri = request.ToUri();
-        // // try
-        // // {
-        // //     BrowserUtil.Open(uri);
-        // // }
-        // // catch (Exception)
-        // // {
-        // //     // TODO: notify user somehow?
-        // // }
+        try
+        {
+            BrowserUtil.Open(loginRequest.ToUri());
+        }
+        catch (Exception)
+        {
+            // TODO: notify user somehow?
+            return;
+        }
 
-        // await tcs.Task;
-    //}
+        await tcs.Task;
+    }
 
-    // private async Task<SpotifyClient> GetSpotifyClient(string clientId)
-    // {
-        // var json = await File.ReadAllTextAsync(_credentialsPath);
-        // var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
+    private async Task<SpotifyClient> GetSpotifyClient(string clientId)
+    {
+        var json = await File.ReadAllTextAsync(_credentialsPath);
+        var token = JsonConvert.DeserializeObject<PKCETokenResponse>(json);
 
-        // var authenticator = new PKCEAuthenticator(clientId!, token!);
-        // authenticator.TokenRefreshed += (sender, token) => File.WriteAllText(_credentialsPath, JsonConvert.SerializeObject(token));
+        var authenticator = new PKCEAuthenticator(clientId!, token!);
+        authenticator.TokenRefreshed += (sender, token) => File.WriteAllText(_credentialsPath, JsonConvert.SerializeObject(token));
 
-        // var config = SpotifyClientConfig.CreateDefault()
-        //     .WithAuthenticator(authenticator);
+        var config = SpotifyClientConfig.CreateDefault()
+            .WithAuthenticator(authenticator);
 
-        // return new SpotifyClient(config);
-        // return null;
-    // }
+        return new SpotifyClient(config);
+    }
 }
