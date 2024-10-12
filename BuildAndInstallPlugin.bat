385e837e3a2b18383501cde046786ea61a04d3c2
eldossjogy <eldossjogy@gmail.com>
2024-10-12 18:09:11 -0400

    Fixed path in bat file

--- BuildAndInstallPlugin.bat
+++ BuildAndInstallPlugin.bat
@@ -1,5 +1,5 @@
 
 dotnet build -c Release
 taskkill /F /IM PowerToys.exe
-robocopy /e /w:5 "%~dp0\bin\Release\net8.0-windows" "C:\Program Files\PowerToys\RunPlugins\PowerToys-Run-Spotify"
+robocopy /e /w:5 "%~dp0\bin\Release\net8.0-windows" "C:\Program Files\PowerToys Run\Plugins\PowerToys-Run-Spotify"
 start "" "C:\Program Files\PowerToys\PowerToys.exe"
