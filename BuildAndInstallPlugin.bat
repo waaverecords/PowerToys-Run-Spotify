6200237ff657a9be6f8c66487dae16859685581b
Harv <HarvT@pm.me>
2025-02-19 23:41:06 +0000

    Improved the build bat file

--- BuildAndInstallPlugin.bat
+++ BuildAndInstallPlugin.bat
@@ -1,5 +1,7 @@
-
+@echo on
+cd "%~dp0\"
 dotnet build -c Release
 taskkill /F /IM PowerToys.exe
 robocopy /e /w:5 "%~dp0\bin\Release\net9.0-windows" "C:\Program Files\PowerToys\RunPlugins\PowerToys-Run-Spotify"
 start "" "C:\Program Files\PowerToys\PowerToys.exe"
+pause
